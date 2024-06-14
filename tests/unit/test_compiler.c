1c86b08b0ad7215b2c61dcdca39bd2611f0845b1
larmix <larmix6@gmail.com>
2024-06-14 12:31:32 +0300

    Add tests for compiling strings
    Add to the expression compilation tests strings, interpolated strings,
    and string concatenation.

--- tests/unit/test_compiler.c
+++ tests/unit/test_compiler.c
@@ -6,31 +6,27 @@
 #include "compiler.c"
 
 /** Tests that math expressions compile properly.*/
-PRIVATE_TEST_CASE(test_compile_math_expr) {
+PRIVATE_TEST_CASE(test_compile_expr) {
     ZmxProgram program = create_zmx_program("no_file", false);
-    char *source = "3 + 4 * --9 * 2 / 7 ** 6 / -2;";
-    Lexer lexer = create_lexer(&program, source);
-    ASSERT_TRUE(lex(&lexer));
-    Parser parser = create_parser(&program, lexer.tokens);
-    ASSERT_TRUE(parse(&parser));
-    Compiler compiler = create_compiler(&program, parser.ast, true);
-    ASSERT_TRUE(compile(&compiler));
+    char *source = "3 + 4 * --9 * 2 / 7 ** 6 / -2;"
+        "$\"start {1 + 2} middle {'inner' + ' string'}\" + \" end.\";";
+    FuncObj *compiledFunc = compile_source(&program, source, true);
+    ASSERT_NOT_NULL(compiledFunc);
 
     const u8 expected[] = {
         OP_LOAD_CONST, 0, OP_LOAD_CONST, 1, OP_LOAD_CONST, 2, OP_MINUS, OP_MINUS, OP_MULTIPLY,
         OP_LOAD_CONST, 3, OP_MULTIPLY, OP_LOAD_CONST, 4, OP_LOAD_CONST, 5, OP_EXPONENT, OP_DIVIDE,
-        OP_LOAD_CONST, 6, OP_MINUS, OP_DIVIDE, OP_ADD, OP_POP, OP_END
+        OP_LOAD_CONST, 6, OP_MINUS, OP_DIVIDE, OP_ADD, OP_POP,
+        OP_LOAD_CONST, 7, OP_LOAD_CONST, 8, OP_LOAD_CONST, 9, OP_ADD, OP_AS, 3, OP_LOAD_CONST, 10,
+        OP_LOAD_CONST, 11, OP_FINISH_STRING, 1, OP_LOAD_CONST, 12, OP_FINISH_STRING, 1, OP_ADD,
+        OP_AS, 3, OP_FINISH_STRING, 4, OP_LOAD_CONST, 13, OP_FINISH_STRING, 1, OP_ADD, OP_POP,
+        OP_END
     };
-    ASSERT_BYTES_EQUAL(compiler.func->bytecode.data, expected, compiler.func->bytecode.length);
-    
-    free_lexer(&lexer);
-    free_all_nodes(&program);
-    free_parser(&parser);
-    free_compiler(&compiler);
+    ASSERT_BYTES_EQUAL(compiledFunc->bytecode.data, expected, compiledFunc->bytecode.length);
     free_zmx_program(&program);
 }
 
 /** Tests compiler.c. */
 void test_compiler() {
-    TEST(test_compile_math_expr);
+    TEST(test_compile_expr);
 }
